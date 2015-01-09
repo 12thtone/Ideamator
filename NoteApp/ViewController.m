cb04039c68a84cfe525ed8f2f8215822e47a9521
Matt Maher <mattmaher@Matts-MacBook-Pro.local>
2015-01-09 13:45:29 -0500

    Initial Commit

---
+++ NoteApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  NoteApp
+//
+//  Created by Matt Maher on 1/9/15.
+//  Copyright (c) 2015 Matt Maher. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
